af8cc679775227e198c4e8cc147f4c359dafd8a9
Miguel T Rivera <mrivera108@outlook.com>
2021-02-17 14:43:35 -0800

    Pass tests: setup, deposit, deposit_no_description

--- budget.py
+++ budget.py
@@ -1,6 +1,11 @@
 class Category:
-    pass
+    def __init__(self, name):
+        self.name = name
+        self.ledger = []
+        self.balance = 0
 
+    def deposit(self, amount, description=''):
+        self.ledger.append({amount: amount, description: description})
 
 def create_spend_chart(categories):
     pass
