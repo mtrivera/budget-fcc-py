a0ece285bc68e3da4462b6a91ad2dee094628a7d
Miguel T Rivera <mrivera108@outlook.com>
2021-02-17 17:29:51 -0800

    Complete most of Category class

--- budget.py
+++ budget.py
@@ -4,8 +4,36 @@ class Category:
         self.ledger = []
         self.balance = 0
 
+    # TODO: Implement magic method '__str__' for printing a budget object
+
     def deposit(self, amount, description=''):
-        self.ledger.append({amount: amount, description: description})
+        self.ledger.append({"amount": amount, "description": description})
+        self.set_balance(amount)
+
+    def withdraw(self, amount, description=''):
+        if self.check_funds(amount):
+            self.ledger.append({"amount": -abs(amount), "description": description})
+            self.set_balance(-abs(amount))
+            return True
+        return False
+
+    def transfer(self, amount, category):
+        if isinstance(category, Category):
+            if self.check_funds(amount):
+                self.withdraw(amount, f'Transfer to {category.name}')
+                category.deposit(amount, f'Transfer from {self.name}')
+                return True
+            return False
+
+    def set_balance(self, amount):
+        self.balance += amount
+
+    def get_balance(self):
+        return self.balance
+
+    def check_funds(self, amount):
+        return self.get_balance() >= amount
+
 
 def create_spend_chart(categories):
     pass
