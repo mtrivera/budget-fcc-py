e67be1c34e2399216ee32d84ad3b2d8e23ce77f2
Miguel T Rivera <mrivera108@outlook.com>
2021-02-20 14:06:38 -0800

    Pass test_create_spend_chart

--- budget.py
+++ budget.py
@@ -45,6 +45,65 @@ class Category:
     def check_funds(self, amount):
         return self.get_balance() >= amount
 
-
+#TODO: Refactor and comment
 def create_spend_chart(categories):
-    pass
+    budgets = categories.copy()
+    result = ["Percentage spent by category\n"]
+    withdrawals, percents = [], []
+    first_category_index, second_category_index, third_category_index = 5, 8, 11
+
+    for b in budgets:
+        withdrawals.append(abs((b.ledger[1]["amount"])))
+
+    for w in withdrawals:
+        percents.append(get_percent(w, withdrawals))
+
+    for i in range(100, -10, -10):
+        result.append(f"{str(i).rjust(3, ' ')}|")
+        for j in range(4, 14):
+            # First category
+            if j == first_category_index and percents[0] >= i:
+                result.append('o')
+            # Second category
+            elif j == second_category_index and percents[1] >= i:
+                result.append('o')
+            # Third category
+            elif j == third_category_index and percents[2] >= i:
+                result.append('o')
+            else:
+                result.append(' ')
+        result.append('\n')
+
+    result.append('    ' + '-' * 10 + '\n')   # create dash line
+
+    largest_name = max(len(c.name) for c in categories)
+
+    first_name, second_name, third_name = budgets[0].name, budgets[1].name, budgets[2].name
+
+    names = update_names([first_name, second_name, third_name], largest_name)
+
+    for i in range(largest_name):
+        for a in range(14):
+            if a == first_category_index:
+                result.append(names[0][i])
+            elif a == second_category_index:
+                result.append(names[1][i])
+            elif a == third_category_index:
+                result.append(names[2][i])
+            else:
+                result.append(' ')
+        result.append("\n")
+
+    return ("".join(result)).rstrip('\n')
+
+def update_names(names, largest):
+    result = []
+    for name in names:
+        if len(name) >= largest:
+            result.append(name)
+        else:
+            result.append(name + (largest - len(name)) * ' ')
+    return result
+
+def get_percent(num, list):
+    return int(num / sum(list) * 100)
